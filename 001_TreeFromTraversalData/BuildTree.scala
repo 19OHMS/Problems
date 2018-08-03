02f33aa81e75d2a8f0a689751ab2107474221ae1
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-03 10:13:33 -0400

    Add "Build Tree from traversal Data" problem.

---
+++ 001_TreeFromTraversalData/BuildTree.scala
@@ -0,0 +1,11 @@
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
+
+object Solution {
+  def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
+        
+  }
+}
---
+++ 001_TreeFromTraversalData/buildTree.py
@@ -0,0 +1,14 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        
